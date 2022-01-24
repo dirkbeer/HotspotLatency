61824218e7d92c8c10b46a24db9e1ff8403a7f53
dirkbeer <rdbeer@pm.me>
2022-01-23 19:47:24 -0800

    now gets nmap time to three hotspots

--- get_helium_latency.sh
+++ get_helium_latency.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-date | tr '\n' ' ' >> /home/dirk/helium_latency.txt
+date | tr '\n' ' ' >> /home/dirk/hotspot_latency.txt
 
+# buffalo
+nmap -Pn -p 44158 99.171.178.180 | grep "Host is" | \
+sed 's/Host is up (//' | sed 's/s latency).//' | \
+tr '\n' ' ' \
+>> /home/dirk/hotspot_latency.txt
+
+# beaver
 nmap -Pn -p 44158 68.8.113.7 | grep "Host is" | \
 sed 's/Host is up (//' | sed 's/s latency).//' | \
-sed 's/\n/ /' \
->> /home/dirk/helium_latency.txt
+tr '\n' ' ' \
+>> /home/dirk/hotspot_latency.txt
 
+# spider
+nmap -Pn -p 44158 68.107.0.69 | grep "Host is" | \
+sed 's/Host is up (//' | sed 's/s latency).//' \
+>> /home/dirk/hotspot_latency.txt
